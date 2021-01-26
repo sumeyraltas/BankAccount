f6afc732e671aab7368f510d6f03093845f37689
sumeyraltas <74320640+sumeyraltas@users.noreply.github.com>
2021-01-26 11:03:24 +0300

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,40 @@
+
+public class Bank {
+
+	public String bankName;
+	public Address bankAddress;
+	
+	public Bank(String bankName, Address bankAddress) {
+		
+		this.bankName = bankName;
+		this.bankAddress = bankAddress;
+	}
+
+	
+
+	public String toString() {
+		return "Bank [Bank Name= " + bankName + ", Bank Address= " + bankAddress + "]";
+	}
+
+
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+
+	public Address getBankAddress() {
+		return bankAddress;
+	}
+
+	public void setBankAddress(Address bankAddress) {
+		this.bankAddress = bankAddress;
+	}
+	
+	
+	
+	
+}
