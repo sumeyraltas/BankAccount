f87e5fe745f14b5ca600560230f800399d74d9d1
sumeyraltas <74320640+sumeyraltas@users.noreply.github.com>
2021-01-26 11:18:24 +0300

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,7 +1,5 @@
-
 public class Person {
-	
-	
+
 	public String firstName;
 	public String lastName;
 	public Address address;
@@ -15,48 +13,33 @@ public class Person {
 		this.birthday = birthday;
 	}
 
-
-
-
 	public String toString() {
 		return "Person [First Name= " + firstName + ", Last Name= " + lastName +  ", Birthday= "
 				+ birthday  + ", Address= " + address + "]";
 	}
 
-
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 	public String getLastName() {
 		return lastName;
 	}
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
 	public Address getAddress() {
 		return address;
 	}
-
 	public void setAddress(Address address) {
 		this.address = address;
 	}
-
 	public String getBirthday() {
 		return birthday;
 	}
-
 	public void setBirthday(String birthday) {
 		this.birthday = birthday;
 	}
-	
-	
-
 }
